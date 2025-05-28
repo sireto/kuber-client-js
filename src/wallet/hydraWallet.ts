f21e10cbe583d97aa00c9b1d27f506930b8a0773
Reeshav Acharya <reeshav99acharya@gmail.com>
2025-05-28 16:52:28 +0545

    Create hydra wallet class

--- src/wallet/hydraWallet.ts
+++ src/wallet/hydraWallet.ts
@@ -1,8 +1,13 @@
-import { CIP30Wallet } from "libcardano/libcardano-wallet/cip30/wallet";
+import { CIP30Wallet } from "libcardano-wallet//cip30/wallet";
 import { KuberHydraService } from "../service/kuberHydraService";
+import { ShelleyWallet } from "libcardano/cardano/primitives/address";
 
 export class HydraWallet extends CIP30Wallet {
-  constructor(hydraService: KuberHydraService) {
-    super(hydraService, hydraService);
+  constructor(
+    hydraService: KuberHydraService,
+    shelleyWallet: ShelleyWallet,
+    network: 0 | 1
+  ) {
+    super(hydraService, hydraService, shelleyWallet, network);
   }
 }
